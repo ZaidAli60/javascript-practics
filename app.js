4180752f6057ca60a07c86cd1634e18d1983f6b3
Zaid ali <65037315+ZaidAli60@users.noreply.github.com>
2021-11-09 16:41:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,146 @@
+// let a = prompt("enter your age", "Your age is");
+// alert(a);
+
+// var a = +prompt("Enter your value", "Your value is");
+// var b = a + 10;
+
+
+// console.log("this is output " + b)
+
+// var x = "Enter value";
+// var y = "Your value";
+// var a = +prompt(x,y);
+// var b = a + 10;
+
+
+// console.log("this is output " + b)
+
+
+
+//  var age = +prompt("Enter your age ");
+
+//  var gender = prompt("male");
+
+
+//  if (age > 18 && gender == "male") {
+//      alert("your are allowed ");
+//  }
+//   else{
+//      alert("Your are not allowed");
+//  }
+
+
+// var a =10;
+// var b = 5;
+// var c =25;
+// var d = 20;
+
+// if (a+b !== c-d) {
+//     alert("correct");
+// } else {
+//     alert("incorrect");
+// }
+
+// var percentage = +prompt("Enter your percentage");
+// if (percentage >=80 && percentage <=100  ) {
+//     alert("A+");
+// }
+// else if(percentage >=70 && percentage <=80  ) {
+//     alert("A");
+// }
+// else if(percentage >=60 && percentage <=70  ) {
+//     alert("B");
+// }
+// else if(percentage >=50 && percentage <=60  ) {
+//     alert("C");
+// }
+// else if(percentage >=33 && percentage <=40 ) {
+//     alert("E");
+// }
+// else if(percentage >=0 && percentage <=33 ) {
+//     alert("F");
+// }
+// else {
+//     alert("You didnot write correct percentage")
+// }
+
+// var student =["zaid","ali","zain","fasial"];
+
+// console.log("Wellcome" + student[3]);
+
+
+// var names = [];
+
+// names[2] = "zaid";
+// names[3] = "ali";
+// names[0] = "zain";
+
+
+// console.log(names);
+
+
+// var students = ["zaid","zain","ali"];
+
+// // students.pop()
+// students.push("faisal","Bilal");
+// console.log(students);
+
+
+// var students = ["zaid","zain","ali"];
+
+// students.shift();
+// // students.unshift("faisal","Bilal");
+// console.log(students);
+
+
+// var students = ["zaid","zain","ali","faisal","bilal"];
+// students.splice(2,3,"Bilal");
+
+// console.log(students);
+
+// var students = ["zaid","zain","ali","faisal","bilal"];
+// var copyStudents = students.slice(0,3);
+// console.log(copyStudents);
+// console.log(students);
+
+
+
+//   practics Function 
+
+
+// function firstName (fname,lname){
+//     document.write("HelloWorld" + " " + fname + "<br> " + lname + "<br>");
+// }
+// function sum(a,b) {
+//     document.write(a + b);
+// }
+
+
+// sum(100,100);
+// firstName("zaidali","zainali");
+// sum(100,100);
+
+// function fullName(fname,lname) {
+//     var a = fname + "-" + lname;
+//     return a;
+// }
+
+// var fn = fullName("Zaid Ali","Zain Ali");
+// document.write(fn);
+
+
+// function sum(math,english,urdu) {
+//     var a = math+english+urdu;
+//     return a;
+// }
+// function percentage(tt) {
+//     var per = tt/300 * 100;
+//     document.write(per);
+// }
+
+// var fn = sum(80,80,80);
+// percentage(fn);
+
+
+
+
